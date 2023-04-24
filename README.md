4ec2cee52bdb4ac18ae8ca985bf6f6a5dfdcc302
sdfields <spencerdfields@gmail.com>
2023-04-24 18:30:40 -0500

    final push before submission

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # SDF-Portfolio
+
+# Description
+
+This is my first attempt at building my portfolio in React.  
+I would like to add a few more features and styling changes, but it is functional and I am happy with it so far.
+
+# Dependencies
+The dependencies for this application are listed in the package.json file.
+
+# Use
+To view the portfolio navigate to the section you would like to view using the Navigation Bar in the top right.  
+There are also links to my email, social media, and resume on the left side.
+
+# License
+
+MIT License
+
+# GitHub
+
+https://github.com/sdfields/SDF-Portfolio
+
+# Deployed Application
+
+Link
+
+Here is a list of changes I would like to make to my portfolio for future reference:
+* Add resume to "Resume" button
+* Update styling to be more cohesive
+* Add projects that are more indicative of what I have learned
--- sdf-portfolio/src/components/Skills.jsx
+++ sdf-portfolio/src/components/Skills.jsx
@@ -1,65 +1,68 @@
-import React from 'react'
-import CSS from '../assets/css.png'
-import GitHub from '../assets/github.png'
-import HTML from '../assets/html.png'
-import JavaScript from '../assets/javascript.png'
-import Mongo from '../assets/mongo.png'
-import Node from '../assets/node.png'
-import ReactImage from '../assets/react.png'
-import Tailwind from '../assets/tailwind.png'
-
+import React from "react";
+import CSS from "../assets/css.png";
+import GitHub from "../assets/github.png";
+import HTML from "../assets/html.png";
+import JavaScript from "../assets/javascript.png";
+import Mongo from "../assets/mongo.png";
+import Node from "../assets/node.png";
+import ReactImage from "../assets/react.png";
+import Tailwind from "../assets/tailwind.png";
 
 const Skills = () => {
   return (
-    <div name = 'skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-        {/*Main Container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
-                <p className='text-4xl font-bold inline border-b-4 border-[#c1ff72]'>
-                    Skills
-                </p>
-                    <p className='py-4'>
-                    These are the technologies I have used in my studies.
-                    </p>
-            </div>
-            {/*Container for Icons*/}
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                <div className='shadow-md shadow-gray-700 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={CSS} alt = "CSS Logo"/>
-                    <p>CSS</p>
-                </div>
-                <div className='shadow-md shadow-gray-700 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={GitHub} alt = "GitHub Logo"/>
-                    <p>GitHub</p>
-                </div>
-                <div className='shadow-md shadow-gray-700 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={HTML} alt = "HTML Logo"/>
-                    <p>HTML</p>
-                </div>
-                <div className='shadow-md shadow-gray-700 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={JavaScript} alt = "JavaScript Logo"/>
-                    <p>JavaScript</p>
-                </div>
-                <div className='shadow-md shadow-gray-700 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={Mongo} alt = "Mongo Logo"/>
-                    <p>Mongo</p>
-                </div>
-                <div className='shadow-md shadow-gray-600 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={Node} alt = "Node Logo"/>
-                    <p>Node</p>
-                </div>
-                <div className='shadow-md shadow-gray-600 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={ReactImage} alt = "React Logo"/>
-                    <p>React</p>
-                </div>
-                <div className='shadow-md shadow-gray-600 hover:scale-110 duration-600'>
-                    <img className='w-20 mx-auto' src={Tailwind} alt = "Tailwind Logo"/>
-                    <p>Tailwind</p>
-                </div>
-            </div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/*Main Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-[#c1ff72]">
+            Skills
+          </p>
+          <p className="py-4">
+            These are the technologies I have used in my studies.
+          </p>
         </div>
+        {/*Container for Icons*/}
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS Logo" />
+            <p>CSS</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={GitHub} alt="GitHub Logo" />
+            <p>GitHub</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML Logo" />
+            <p>HTML</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-600">
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="JavaScript Logo"
+            />
+            <p>JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-gray-700 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={Mongo} alt="Mongo Logo" />
+            <p>Mongo</p>
+          </div>
+          <div className="shadow-md shadow-gray-600 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={Node} alt="Node Logo" />
+            <p>Node</p>
+          </div>
+          <div className="shadow-md shadow-gray-600 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={ReactImage} alt="React Logo" />
+            <p>React</p>
+          </div>
+          <div className="shadow-md shadow-gray-600 hover:scale-110 duration-600">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind Logo" />
+            <p>Tailwind</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
